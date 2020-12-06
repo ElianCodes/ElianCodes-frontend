ea36f7ada6320ced0070cb9431e11cfc12c27720
Elian Van Cutsem <elianvancutsem@gmail.com>
2020-12-06 18:53:04 +0100

    ✨adding more projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,8 +3,8 @@
     <div class="container-fluid px-md-0">
       <div class="row no-gutters justify-content-center pb-5">
         <div class="col-md-12 heading-section text-center ftco-animate">
-          <h2 class="mb-4">Projects</h2>
-          <p class="text-dark">These are some of my latest projects</p>
+          <h2 class="mb-4">Personal Projects</h2>
+          <p class="text-dark">Next to working at vBridge and studying, I have a lot of other projects going on. You can find some below.</p>
         </div>
       </div>
       <div class="row no-gutters">
@@ -36,14 +36,15 @@ export default {
     return {
       currentPage: 1,
       projectList: [
+        { id: 8, tag: "Webdesign & wordpress", category: "webdevelopment", img: "https://i.imgur.com/7ixAE5E.jpg", link: "https://www.de-grill.be" },
         { id: 7, tag: "Front-end in wix", category: "webdevelopment", img: "https://i.imgur.com/EgL9lFc.jpeg", link: "https://www.cambussa.be" },
         { id: 6, tag: "Back-end in Symfony", category: "Api development", img: "https://i.imgur.com/MA0lOJi.jpeg", link: "https://seidi-clothing.com/webshop/index.php" },
         { id: 5, tag: "Front-End in React", category: "Webdevelopment", img: "https://i.imgur.com/wHSkKUQ.jpg", link: "https://www.jinsscoutsmerchtem.be" },
         { id: 4, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/Uhtabjz.jpg", link: "https://www.stijnbogemans.be" },
         { id: 3, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/cVqUkmp.jpeg", link: "https://www.evaderidder.com" },
-        { id: 2, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/LEwsMh5.jpg", link: "https://www.kalathea.be" },
+        //{ id: 2, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/LEwsMh5.jpg", link: "https://www.kalathea.be" },
         { id: 1, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/vc6oAlJ.jpg", link: "https://www.lartgalerie.be" },
-        { id: 0, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/8dQG2QY.jpg", link: "https://www.sportify.be" },
+        //{ id: 0, tag: "Webdesign & wordpress", category: "Webdevelopment", img: "https://i.imgur.com/8dQG2QY.jpg", link: "https://www.sportify.be" },
       ]
     }
   },
