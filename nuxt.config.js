393be3fe9752526c538bb2d5e3e24fac12767382
Elian Van Cutsem <elianvancutsem@gmail.com>
2021-01-23 12:14:13 +0100

    🍱 adding favicon and meta desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,16 +4,16 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'elianvancutsem',
+    title: 'Elian Van Cutsem',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Elian Van Cutsem' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
