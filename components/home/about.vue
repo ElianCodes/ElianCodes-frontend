b46a5e4dc55eca5320d8ba5480ff70c6b2fad335
Elian Van Cutsem <elianvancutsem@gmail.com>
2021-02-04 12:47:39 +0100

    💄 better about me section

--- components/home/about.vue
+++ components/home/about.vue
@@ -16,37 +16,24 @@
         <div class="col-md-6 col-lg-6 pl-md-5 py-5">
           <div class="row justify-content-start pb-3">
             <div class="col-md-12 heading-section ftco-animate">
-              <h2 class="big">About</h2>
-              <h3 class="mb-4">About Me</h3>
-              <p class="text-dark">
-                I've been studying Computer Science for almost 2 years now. I'm
-                still learning lots of new technologies and skills everyday. I'm
-                trying to better my skills by building websites for friends and
-                family and trying out fun projects. Mostly I'm building websites
-                for clients through technologies like WordPress, but I'm still
-                learning
-                <a v-for="item in resumeItems" :key="item.name"
-                  >{{ item.name }},
-                </a>
-                probably way more.
+              <h2 class="display-3">About</h2>
+              <p class="text-muted">
+                <span class="mr-1 icon">👋</span>&nbsp;Hello there! My name is
+                Elian Van Cutsem. I'm a Student and software engineer from
+                belgium. On my personal site you'll find more info about myself
+                and my projects. At the moment I'm doing an internship @<a
+                  href="https://vbridge.eu"
+                  target="_blank"
+                  >vBridge</a
+                >
+                while finishing my Computer Science degree @<a
+                  href="https://www.odisee.be/en"
+                  target="_blank"
+                  >Odisee</a
+                >. I'm still learning lots of new technologies and skills
+                everyday. On this site I try to blog about my findings and
+                experiences and show off some projects I worked on.
               </p>
-              <ul class="about-info mt-4 px-md-0 px-2 text-dark">
-                <li class="d-flex">
-                  <span>Name:&nbsp;</span><span>Elian Van Cutsem</span>
-                </li>
-                <li class="d-flex">
-                  <span>Date of birth:&nbsp;</span><span>July 09, 1999</span>
-                </li>
-                <li class="d-flex">
-                  <span>Location:&nbsp;</span><span>Ghent, Belgium</span>
-                </li>
-                <li class="d-flex">
-                  <span>Email:</span>&nbsp;<a
-                    href="mailto:elianvancutsem@gmail.com"
-                    ><span>elianvancutsem@gmail.com</span></a
-                  >
-                </li>
-              </ul>
             </div>
           </div>
         </div>
@@ -72,4 +59,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.icon {
+  font-size: 1.6rem;
+}
+</style>
