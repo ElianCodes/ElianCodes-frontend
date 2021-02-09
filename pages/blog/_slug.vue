64c214893e3f88917f17fd8121e22e4de63dde7a
Elian Van Cutsem <elianvancutsem@gmail.com>
2021-02-09 21:43:40 +0100

    🔍 better SEO on blogposts

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -18,6 +18,18 @@ export default {
 
     return { article }
   },
+  head() {
+    return {
+      title: this.article.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.article.description },
+        { hid: 'og:title', property: 'og:title', content: this.article.title },
+        { hid: 'og:description', property: 'og:description', content: this.article.description },
+        { hid: 'twitter:title', name: 'twitter:title', content: this.article.title },
+        { hid: 'twitter:description', name: 'twitter:description', content: this.article.description }
+      ]
+    }
+  },
   methods: {
     formatDate(date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
