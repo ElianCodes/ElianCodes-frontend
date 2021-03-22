4b77167c2c0164a8ac0185cd45de63f1cee8746f
Elian Van Cutsem <elianvancutsem@gmail.com>
2021-03-22 18:12:09 +0100

    📝 blogpost about Netlify

--- components/blog/articleFooter.vue
+++ components/blog/articleFooter.vue
@@ -8,13 +8,13 @@
       </button>
       <span>{{ this.formatedLikes() }}</span>
     </div>-->
-    <a href="/blog.xml" class="blog-action">
+    <a href="/blog.xml" class="blog-action" title="Subscribe to the RSS Feed">
       <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
       </svg>
     </a>
     <!--<a  class="twitter-share-button" data-show-count="false">Tweet</a>-->
-    <a :href="this.tweetLink" class="blog-action">
+    <a :href="this.tweetLink" class="blog-action" title="Share this article on Twitter">
       <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
       </svg>
