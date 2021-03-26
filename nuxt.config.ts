d188acef377440e9d0800b2a733ea2f7c1360ed5
Elian Van Cutsem <elianvancutsem@gmail.com>
2021-03-27 00:29:08 +0100

    📈adding coil support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -104,6 +104,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
+      { name: "monetization", content: "$ilp.uphold.com/gH9RGFW9ijRA" },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Elian Van Cutsem' },
     ],
