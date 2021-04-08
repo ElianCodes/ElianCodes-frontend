8682de0fbc4e7ca49a9724cbdb9315682da0ff91
Elian Van Cutsem <elianvancutsem@gmail.com>
2021-04-08 10:44:41 +0200

    📈adding buy me a coffee button on Blog

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -23,6 +23,7 @@
         </div>
       </div>
     </section>
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="elianvancutsem" data-description="Buy me a coffee!" data-message="" data-color="#79D6B5" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
   </section>
 </template>
 
