7f5e760459b264096479060d60eb034c2ce880c0
Elian Van Cutsem <hello@elian.codes>
2021-09-03 07:05:03 +0200

    ☕ Introducing astro

---
+++ newaf/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    mode: 'jit',
+    purge: {
+      enabled: true,
+      content: [
+        './public/**/*.html',
+        './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
+      ],
+    },
+    darkMode: 'class',
+    theme: {
+      extend: {
+        animation: {
+          'spin-slow': 'spin 3s linear infinite',
+        }
+      },
+    },
+    variants: {
+      typography: ["responsive", "dark"],
+      extend: {
+        backgroundColor: ['dark', 'responsive'],
+      },
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography')
+    ],
+  }
+  
