612f888f29c6eed31bed1d352b9c852c2ba73bf0
Elian Van Cutsem <elian.vancutsem@vbridge.eu>
2021-09-22 11:46:35 +0200

    ✨ introducing .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { ProjectsModule } from './projects/projects.module';
 import { CertificationsModule } from './certifications/certifications.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProjectsModule, CertificationsModule],//MongooseModule.forRoot(connectionString)],
+  imports: [ProjectsModule, CertificationsModule, ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(process.env.DB_STRING)],
   controllers: [AppController],
   providers: [AppService],
 })
