27a499b8ff3810e3b696ce925e7e360113365c43
Elian Van Cutsem <hello@elian.codes>
2021-09-22 07:31:33 +0200

    🗃️ Adding Mongoose and MongoDB COnnection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(connectionString)],
   controllers: [AppController],
   providers: [AppService],
 })
