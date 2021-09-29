6d62ff6dfa54939020a8b346885444f927be12ec
Elian Van Cutsem <elian.vancutsem@vbridge.eu>
2021-09-29 17:20:20 +0200

    ♻️ add footer technologies at random

--- frontend/src/components/layout/DefaultFooter.tsx
+++ frontend/src/components/layout/DefaultFooter.tsx
@@ -15,14 +15,18 @@ const initAnimation = () => {
 }
 
 export default function DefaultFooter({ overlay, linkColor, slug }) {
-    const technologies = ['Astro', 'TailwindCSS']
+    const technologies = ['Astro', 'TailwindCSS', 'Svelte', 'Preact', 'NestJS', 'AOS', 'Vue', 'Sass', 'TypeScript', 'JavaScript']
     useEffect(() => {
       initAnimation()
     }, [])
     return (
       <footer class={overlay ? 'overlay' : ''}>
-        <p>Built with <a href="/technologies/" class={ linkColor ? linkColor : "use-color"}>Astro</a>, <a href="/technologies/" class={ linkColor ? linkColor : "use-color"}>TailwindCSS</a> &amp; { slug === 'coffee' ? (<>🍵</>) : (<a href="/coffee/">☕</a>) }</p>
-        { overlay ? (<p class="overlay-text"><a rel="nofollow noreferer" href="https://www.preactjs.com" target="_blank">Preact</a></p>) : null }
+        <p>
+          Built with <a href="/technologies/" class={ linkColor ? linkColor : "use-color"}>{technologies[(Math.floor(Math.random() * technologies.length))]}</a> &amp; <a href="/coffee/">☕</a>
+        </p>
+        { overlay ? (
+            <p class="overlay-text"><a rel="nofollow noreferer" href="https://www.preactjs.com" target="_blank">Preact</a></p>
+        ) : null }
       </footer>
     );
   }
