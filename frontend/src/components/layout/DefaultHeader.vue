f9dae82d486e69e7be18324409b14c9b79f2055a
Elian Van Cutsem <hello@elian.codes>
2021-10-16 21:02:09 +0200

    💄 add menubtn color back

--- frontend/src/components/layout/DefaultHeader.vue
+++ frontend/src/components/layout/DefaultHeader.vue
@@ -23,7 +23,7 @@
 			<div class="mobile-menu">
 				<button class="toggle-menu" @click="openMenu" v-if="!menuOpen">
 					<span class="sr-only">Open Menu</span>
-					<svg class="w-8 h-8 use-color" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+					<svg :class="['w-8 h-8', menuColor ? menuColor : 'use-color']" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
 				</button>
 				<div class="mobile-list" v-if="menuOpen">
 					<div class="use-color">
