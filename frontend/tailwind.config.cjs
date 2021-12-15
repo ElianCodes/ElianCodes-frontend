498fb5979f85247f61d2cd37dfa47a78dff41758
Elian Van Cutsem <hello@elian.codes>
2021-12-15 21:11:49 +0100

    ♻️ Add complete redesign for first page

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'primary-green': colors.emerald[300],
+        'primary-blue': colors.blue[300],
+        'primary-red': colors.red[300],
+        'primary-orange': colors.orange[300],
+        'primary-fuchsia': colors.fuchsia[300],
+      }
+    },
+  },
+  plugins: [],
+}
