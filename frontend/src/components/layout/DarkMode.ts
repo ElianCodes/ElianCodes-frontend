977085cb2c7f41e526b9c407a9f38e6624808d53
Elian Van Cutsem <hello@elian.codes>
2021-12-17 20:44:20 +0100

    💫 optimize darkmode animation and flickering

--- frontend/src/components/layout/DarkMode.ts
+++ frontend/src/components/layout/DarkMode.ts
@@ -1,12 +1,32 @@
+// set this to trigger immediatly
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.documentElement;
-    
     const darkModeActive = () => { return root.classList.contains('dark') }
     
     document.querySelectorAll('button.toggleMode').forEach(btn => {
         btn.addEventListener('click', e => {
             e.preventDefault()
-            darkModeActive() ? root.classList.remove('dark') : root.classList.add('dark')
+            root.classList.add('transition-colors', 'ease-in-out', 'duration-500');
+            darkModeActive() ? setLightMode() : setDarkMode()
+            setTimeout(() => {
+                root.classList.remove('transition-colors', 'ease-in-out', 'duration-500')
+            }, 500)
         })
     })
+
+    const setDarkMode = () => {
+        localStorage.setItem('theme', 'dark')
+        root.classList.add('dark')
+    }
+
+    const setLightMode = () => {
+        localStorage.setItem('theme', 'light')
+        root.classList.remove('dark')
+    }
 })
