737d3584d281036ab1aab149ce12c1d8404097dd
ElianCodes ☕ <hello@elian.codes>
2022-01-22 18:27:13 +0100

    ♻️ fix whitespace

--- frontend/src/layouts/CustomColor.ts
+++ frontend/src/layouts/CustomColor.ts
@@ -1,19 +1,19 @@
 const colors = [
-    { class: 'primary-green', code: '#86EFAC'},
-    { class: 'primary-blue', code: '#93C5FD'},
-    { class: 'primary-red', code: '#FCA5A5'},
-    { class: 'primary-orange', code: '#FDBA74'},
-    { class: 'primary-fuchsia', code: '#F0ABFC'}
+	{ class: 'primary-green', code: '#86EFAC'},
+	{ class: 'primary-blue', code: '#93C5FD'},
+	{ class: 'primary-red', code: '#FCA5A5'},
+	{ class: 'primary-orange', code: '#FDBA74'},
+	{ class: 'primary-fuchsia', code: '#F0ABFC'}
 ]
 
 // set this to trigger immediatly
 if (!('color' in localStorage)) {
-    document.documentElement.style.setProperty('--custom-color', colors[Math.floor(Math.random() * colors.length)].code)
+	document.documentElement.style.setProperty('--custom-color', colors[Math.floor(Math.random() * colors.length)].code)
 } else {
-    document.documentElement.style.setProperty('--custom-color', localStorage.getItem('color'))
+	document.documentElement.style.setProperty('--custom-color', localStorage.getItem('color'))
 }
 
 document.addEventListener('getRandomColor', () => {
-    console.log('got new color!')
-    document.documentElement.style.setProperty('--custom-color', colors[Math.floor(Math.random() * colors.length)].code)
+	console.log('got new color!')
+	document.documentElement.style.setProperty('--custom-color', colors[Math.floor(Math.random() * colors.length)].code)
 })
