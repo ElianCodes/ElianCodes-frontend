0c3cfe85b1415d02728b636dabf9aeb37bd83b55
Elian Van Cutsem <elianvancutsem@gmail.com>
2022-05-04 10:21:03 +0200

    🎉 add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'ignore',
@@ -8,9 +10,7 @@ export default defineConfig({
     port: 3000
   },
   site: 'https://www.elian.codes',
-  integrations: [
-    sitemap({
-      canonicalURL: 'https://www.elian.codes',
-    }),
-  ]
+  integrations: [sitemap({
+    canonicalURL: 'https://www.elian.codes'
+  }), partytown()]
 });
