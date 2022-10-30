d542e963a3466f39e9097c23cdd679c3e8259780
Elian Van Cutsem <hello@elian.codes>
2022-10-30 17:57:28 +0100

    ♻️ improve component codestyle

--- src/components/layout/PickColor.tsx
+++ src/components/layout/PickColor.tsx
@@ -1,46 +1,45 @@
-import { onMount } from "solid-js";
-import { createSignal } from "solid-js";
+import { onMount, createSignal } from "solid-js";
 
 const PickColor = () => {
-  const [canColorBePicked, setColorCanBePicked] = createSignal(false)
-  const [customColorActive, setCustomColorActive] = createSignal(false)
+  const [canColorBePicked, setColorCanBePicked] = createSignal(false);
+  const [customColorActive, setCustomColorActive] = createSignal(false);
 
   onMount(() => {
     if (typeof window !== "undefined") {
       setColorCanBePicked(true);
     }
-  })
+  });
 
   const pickColor = () => {
     const eyeDropper = new EyeDropper();
-      eyeDropper.open().then(result => {
-          const rgbValue = result.sRGBHex
-          document.documentElement.style.setProperty('--custom-color', rgbValue)
-          localStorage.setItem('color', rgbValue)
-          setCustomColorActive(true)
-        }).catch(e => {
-          console.log(e)
+      eyeDropper.open().then((result) => {
+          const rgbValue = result.sRGBHex;
+          document.documentElement.style.setProperty('--custom-color', rgbValue);
+          localStorage.setItem('color', rgbValue);
+          setCustomColorActive(true);
+        }).catch((e) => {
+          console.error(e);
       });
-  }
+  };
 
   const removeColor = () => {
-    localStorage.removeItem('color')
-    setCustomColorActive(false)
-    document.dispatchEvent(new Event('getRandomColor'))
-  }
+    localStorage.removeItem('color');
+    setCustomColorActive(false);
+    document.dispatchEvent(new Event('getRandomColor'));
+  };
 
   return (
     <>
     { canColorBePicked() ? (
         customColorActive() ? (
-          <button onClick={removeColor} title="Remove custom color" type="button" id="clearColor">🗑</button>
+          <button onClick={removeColor} title="Remove custom color" type="button">🗑</button>
         ) : (
-          <button onClick={pickColor} title="Pick a new color" type="button" id="eyeDropper">🔍</button>
+          <button onClick={pickColor} title="Pick a new color" type="button">🔍</button>
         )
     ) : null 
     }
     </>
-  )
-}
+  );
+};
 
 export default PickColor;
