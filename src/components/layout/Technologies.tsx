a25ef191d353e4100478f67d76465078619b6fbc
Elian Van Cutsem <hello@elian.codes>
2022-11-10 18:28:50 +0100

    📝 change description

--- src/components/layout/Technologies.tsx
+++ src/components/layout/Technologies.tsx
@@ -1,8 +1,3 @@
-interface Technology {
-  name: string
-  link: string
-}
-
 const Technologies = ({ technology }) => {
   return (
     <span>Website built with <a class="use-color transition-all ease-in-out duration-300" title={`go to the ${technology.name}`} target="_blank" href={technology.link}>{technology.name}</a> & ☕️</span>
