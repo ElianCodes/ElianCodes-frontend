2a2c703b860f15ba361b3b2e217a1491247e6c92
Elian Van Cutsem <hello@elian.codes>
2022-11-10 12:42:35 +0100

    🚀 add vcf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +10,12 @@ export default defineConfig({
     port: 3000
   },
   site: 'https://www.elian.codes',
-  integrations: [sitemap({
-    canonicalURL: 'https://www.elian.codes'
-  }), partytown(), image(), solidJs()]
+  integrations: [
+    //sitemap({
+    //canonicalURL: 'https://www.elian.codes'
+  //}),
+    image(), solidJs()
+  ],
+  output: "server",
+  adapter: vercel()
 });
