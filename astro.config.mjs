c4abc2f138a196b69e1b213a2b61a970cf6d6920
Elian Van Cutsem <hello@elian.codes>
2022-11-10 13:54:29 +0100

    ⬆️ Upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
