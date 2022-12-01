aca037b8d7d7c2d8ec623b8ad42886706c2b4cb6
Elian Van Cutsem <hello@elian.codes>
2022-12-01 08:13:23 +0100

    ➕ Switch dependency to integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import sitemap from '@astrojs/sitemap';
 import image from "@astrojs/image";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/static";
-
-// https://astro.build/config
 import rome from "astro-rome";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
   site: 'https://www.elian.codes',
   integrations: [sitemap({
     canonicalURL: 'https://www.elian.codes'
-  }), image(), solidJs(), rome()],
+  }), image(), solidJs(), rome(), tailwind()],
   output: "static",
   adapter: vercel()
 });
