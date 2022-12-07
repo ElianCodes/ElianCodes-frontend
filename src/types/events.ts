e62a2fdb80cd6a7515510c6255552a760e1fb90f
Elian Van Cutsem <hello@elian.codes>
2022-12-07 08:59:08 +0100

    ♻️ make Event type handle Firestore

--- src/types/events.ts
+++ src/types/events.ts
@@ -2,7 +2,7 @@ export default interface Event {
   name: string
   title: string
   link: string
-  date: Date | string 
+  date: Date | any 
   abstract: string
   type: string
   blogpost?: string
