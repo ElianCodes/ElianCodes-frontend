a517888aff52f7a3f4ecee91906df8359601ccb1
Elian Van Cutsem <hello@elian.codes>
2023-01-25 09:55:29 +0100

    ♻️ change to pagesGlobToRssItems

--- src/pages/blog.xml.js
+++ src/pages/blog.xml.js
@@ -1,13 +1,13 @@
-import rss from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-const posts = await import.meta.glob('./blog/**/*.md');
-
-export const get = () => rss({
-    title: 'Elian Van Cutsem',
-    description: 'Programming and Frontend related articles and guides',
-    stylesheet: false,
-    items: posts,
-    customData: "<language>en-us</language>",
-    canonicalUrl: "https://www.elian.codes",
-    site: "https://www.elian.codes"
-});
+export async function get(context) {
+    return rss({
+        title: 'Elian Van Cutsem',
+        description: 'Programming and Frontend related articles and guides',
+        stylesheet: false,
+        items: await pagesGlobToRssItems(import.meta.glob('./blog/**/*.{md,mdx}')),
+        customData: "<language>en-us</language>",
+        canonicalUrl: "https://www.elian.codes",
+        site: "https://www.elian.codes"
+    });
+}
