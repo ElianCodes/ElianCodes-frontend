fb769db7d85405a4aee46e88101d1b2da12861a2
Elian Van Cutsem <hello@elian.codes>
2024-05-22 20:39:41 +0200

    feat: enable devtools

--- astro.config.ts
+++ astro.config.ts
@@ -2,28 +2,28 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import db from "@astrojs/db";
+import webVitals from "@astrojs/web-vitals";
 
+// https://astro.build/config
 export default defineConfig({
-	site:
-		process.env.VERCEL_ENV === "production"
-			? "https://www.elian.codes/"
-			: process.env.VERCEL_URL
-			? `https://${process.env.VERCEL_URL}/`
-			: "https://localhost:3000/",
-	trailingSlash: "ignore",
-	integrations: [sitemap(), UnoCSS({ injectReset: true })],
-	output: "static",
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-		speedInsights: {
-			enabled: true,
-		},
-	}),
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  site: process.env.VERCEL_ENV === "production" ? "https://www.elian.codes/" : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : "https://localhost:3000/",
+  trailingSlash: "ignore",
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), db(), webVitals()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    speedInsights: {
+      enabled: true
+    }
+  }),
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
