891ba90a93e26a9e635fe6fc0df6077f849d5bbf
Elian Van Cutsem <hello@elian.codes>
2024-05-22 20:45:38 +0200

    feat: enable remote build

--- astro.config.ts
+++ astro.config.ts
@@ -7,23 +7,30 @@ import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.VERCEL_ENV === "production" ? "https://www.elian.codes/" : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : "https://localhost:3000/",
-  trailingSlash: "ignore",
-  integrations: [sitemap(), UnoCSS({
-    injectReset: true
-  }), db(), webVitals()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    speedInsights: {
-      enabled: true
-    }
-  }),
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+	site:
+		process.env.VERCEL_ENV === "production"
+			? "https://www.elian.codes/"
+			: process.env.VERCEL_URL
+				? `https://${process.env.VERCEL_URL}/`
+				: "https://localhost:3000/",
+	trailingSlash: "ignore",
+	integrations: [
+		sitemap(),
+		UnoCSS({
+			injectReset: true,
+		}),
+		db(),
+		webVitals(),
+	],
+	output: "static",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
