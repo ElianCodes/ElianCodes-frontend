a7b55c5e076e7c07e5fcf87f36996c21d9d85155
Elian Van Cutsem <hello@elian.codes>
2025-10-05 14:42:57 +0200

    chore: update

--- src/pages/v1/generate/og/[slug].png.ts
+++ src/pages/v1/generate/og/[slug].png.ts
@@ -1,8 +1,8 @@
+import { getCollection } from "astro:content";
 import { Resvg, type ResvgRenderOptions } from "@resvg/resvg-js";
 import type { APIRoute } from "astro";
 import satori from "satori";
 import { html as toReactElement } from "satori-html";
-import { getCollection } from "astro:content";
 
 const fontFile = await fetch(
 	"https://og-playground.vercel.app/inter-latin-ext-700-normal.woff",
