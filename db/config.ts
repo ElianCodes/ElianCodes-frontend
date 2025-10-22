d72044b5e1dd5d061e214aa9553f0ddc73a84ab6
Elian Van Cutsem <hello@elian.codes>
2025-10-22 13:55:24 -0500

    feat: insert new logo

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,20 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, NOW } from 'astro:db';
 
-// https://astro.build/db/config
+
+export const Sponsorship = defineTable({
+	columns: {
+		name: column.text(),
+		avatar: column.text(),
+		link: column.text(),
+		description: column.text(),
+		since: column.date({ default: NOW }),
+		ended_at: column.date({ optional: true }),
+		contribution_amount: column.number({ optional: true }),
+	},
+});
+
 export default defineDb({
-  tables: {}
+  tables: {
+		Sponsorship,
+	}
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,36 @@
-import { db } from 'astro:db';
+import { db, Sponsorship } from 'astro:db';
 
-// https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Sponsorship).values([
+		{
+			name: "Biome",
+			avatar: "https://avatars.githubusercontent.com/u/140182603?s=200&v=4",
+			link: "https://github.com/biomejs",
+			description: "Why it's awesome",
+		},
+		{
+			name: "Vite",
+			avatar: "https://avatars.githubusercontent.com/u/65625612?s=200&v=4",
+			link: "https://github.com/vitejs",
+			description: "Why it's awesome",
+		},
+		{
+			name: "Evan You",
+			avatar: "https://avatars.githubusercontent.com/u/499550?v=4",
+			link: "https://github.com/yyx990803",
+			description: "Why it's awesome",
+		},
+		{
+			name: "Josh Goldberg",
+			avatar: "https://avatars.githubusercontent.com/u/3335181?v=4",
+			link: "https://github.com/JoshuaKGoldberg",
+			description: "Why it's awesome",
+		},
+		{
+			name: "Anthony Fu",
+			avatar: "https://avatars.githubusercontent.com/u/11247099?v=4",
+			link: "https://github.com/antfu",
+			description: "Why it's awesome",
+		},
+	]);
 }
