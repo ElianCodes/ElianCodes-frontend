c3100cda23d6f5e16de2bd407f41afe4776fff7d
Elian Van Cutsem <hello@elian.codes>
2026-03-27 13:47:01 +0100

    chore: update config

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,13 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
 	site:
 		process.env.VERCEL_ENV === "production"
 			? "https://www.elian.codes/"
 			: process.env.VERCEL_URL
 				? `https://${process.env.VERCEL_URL}/`
-				: "https://localhost:3000/",
+				: "https://localhost:4321/",
 	trailingSlash: "ignore",
 	integrations: [sitemap()],
 	adapter: vercel(),
