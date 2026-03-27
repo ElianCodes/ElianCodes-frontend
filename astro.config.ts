26b85164e6b2fdf187ee084effc2fd360c224e01
Elian Van Cutsem <hello@elian.codes>
2026-03-27 12:32:19 +0100

    Refresh Astro site layout and component styling

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,22 @@
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
-// import UnoCSS from "unocss/astro";
 import db from "@astrojs/db";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site:
-        process.env.VERCEL_ENV === "production"
-            ? "https://www.elian.codes/"
-            : process.env.VERCEL_URL
-                ? `https://${process.env.VERCEL_URL}/`
-                : "https://localhost:3000/",
-    trailingSlash: "ignore",
-    integrations: [sitemap(), db()],
-    adapter: vercel(),
-    vite: {
-      optimizeDeps: {
-          exclude: ["@resvg/resvg-js"],
-      },
-      plugins: [tailwindcss()],
-    },
+	site:
+		process.env.VERCEL_ENV === "production"
+			? "https://www.elian.codes/"
+			: process.env.VERCEL_URL
+				? `https://${process.env.VERCEL_URL}/`
+				: "https://localhost:3000/",
+	trailingSlash: "ignore",
+	integrations: [sitemap(), db()],
+	adapter: vercel(),
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
